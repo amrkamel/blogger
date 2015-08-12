adb0ac84e5ca54fff7068184595b742ddc8b07e6
Amr Kamel <3amr.kaml@gmail.com>
2015-08-12 16:59:06 +0200

    Implement update article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,13 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
+  before_action :set_article, only: [:show, :update, :destroy, :edit]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find params[:id]
+
   end
 
   def new
@@ -22,13 +23,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    @article.update_attributes article_params
+    if @article.save
+      flash.notice = 'Article has been edited successfully.'
+      redirect_to article_path(@article)
+    else
+      render action: :edit
+    end
+  end
+
   def destroy
-    @article = Article.find params[:id]
     if @article.destroy
-      flash[:notice] = 'Article has been removed successfully'
+      flash.notice = 'Article has been removed successfully.'
       redirect_to articles_path
     else
-      flash[:error] = 'Article could not been deleted.'
       render action: :index
     end
   end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,8 @@ module ArticlesHelper
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
+  def set_article
+    @article = Article.find params[:id] if params[:id]
+  end
 end
