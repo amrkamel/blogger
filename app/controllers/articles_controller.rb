da9f7cb534aedfa2a8ad36059bc15dd97375bc13
Amr Kamel <3amr.kaml@gmail.com>
2015-08-12 16:17:37 +0200

    Implement new article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,24 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find params[:id]
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new article_params
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render action: :new
+    end
   end
 end
